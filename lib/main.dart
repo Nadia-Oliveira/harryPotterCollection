1054359ebe17041ffd631b14c907bdc5ceaf9b9c
Airton Freitas <airton.freitas@localiza.com>
2023-08-22 18:28:38 -0300

    ajuste de pastas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:harry_potter_collection/splash.view.dart';
+import 'package:harry_potter_collection/feature/splash/splash.view.dart';
 
 void main() {
   runApp(const MyApp());
