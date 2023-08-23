6a70f2ed8467d062b4aa8052a9203bf06edd44db
Airton Freitas <airton.freitas@localiza.com>
2023-08-23 17:51:33 -0300

    adiciona animacao splash

--- lib/feature/splash/splash.view.dart
+++ lib/feature/splash/splash.view.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
 import 'package:harry_potter_collection/feature/selection_houses.view.dart';
 
 class SplashView extends StatefulWidget {
@@ -21,7 +23,15 @@ class _SplashViewState extends State<SplashView> {
       ),
     );
 
-    return Image.asset('assets/images/bg_two.png',
-        width: MediaQuery.of(context).size.width);
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset('assets/images/bg_two.png',
+            width: MediaQuery.of(context).size.width),
+        Lottie.asset(
+          'assets/animations/loading.json',
+        )
+      ],
+    );
   }
 }
